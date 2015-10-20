9aa2e68b82cb18a26f385602f64cbde865f0ba14
sebtn <sebtn@users.noreply.github.com>
2015-10-20 16:02:31 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,10 +2,12 @@
 # setwd("~/R prog.coursera")
 
 # makeChacheMatrix is a function list, includes: set, get, getinverse and setinverse.
-# Let's define a matrix with MakechacheMatrix function and let's name it "z":
+# Let's define a matrix with MakecacheMatrix function and let's name it "z":
 # some commands and their results:
 # z$get(); gives back the original matrix.
 # cacheSolve(z); returns the matrix "z" inverse and stores it in the cache for future references.
+# Because "matrix inversion is usually a costly computation and there may be some benefit to caching 
+# the inverse of a matrix rather than computing it repeatedly".
 # z$getinverse; returns the inverse, but with no cache storage function to it.
 # cacheSolve(z); gives back the matrix z inverse in storage. You can see in the screen "getting cache data"
 
